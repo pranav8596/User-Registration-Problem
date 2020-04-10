e5e5e7ba1ff90804ca8c09ebcb310f36ede57cc3
pranav8596 <pige786123@gmail.com>
2020-04-10 16:24:06 +0530

    ADDED: Check the validation of User's Last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,7 +4,7 @@ shopt -s extglob
 #Pattern for First name
 namePattern=^[A-Z]{1}[a-z]{3,}$
 
-#To check Validation for First name
+#To check Validation for User entries
 function checkValidation() {
 	if [[ $1 =~ $2 ]]
 	then
@@ -12,8 +12,10 @@ function checkValidation() {
 	else
 		echo "Invalid"
 	fi
-
 }
 
 read -p "Enter your First name: " firstName
 checkValidation $firstName $namePattern
+
+read -p "Enter your Last name: " lastName
+checkValidation $lastName $namePattern
