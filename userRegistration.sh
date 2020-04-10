1e9b2066990ab72ea24536600e9b9a2bcef318ca
pranav8596 <pige786123@gmail.com>
2020-04-10 18:34:35 +0530

    ADDED: Validation of User's Password with Minimum 8 charaters

--- userRegistration.sh
+++ userRegistration.sh
@@ -10,6 +10,9 @@ emailPattern="^[A-Za-z]{3,}([.|+|_|-]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2
 #Pattern for Mobile Number
 mobilePattern="^[0-9]{1,3}[ ][0-9]{10}$"
 
+#Pattern for Password Rule 1: Minimum 8 chracters
+passwordPattern="^[a-zA-Z]{8,}$"
+
 #To check Validation for User entries
 function checkValidation() {
 	if [[ $1 =~ $2 ]]
@@ -31,3 +34,6 @@ checkValidation $emailAddress $emailPattern
 
 read -p "Enter your Mobile number: " mobileNumber
 checkValidation "$mobileNumber" "$mobilePattern"
+
+read -p "Enter your Password: " password
+checkValidation $password $passwordPattern
